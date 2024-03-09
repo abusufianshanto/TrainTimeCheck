da712c125ea4bc4b568a081b145a6cd97eb251a2
Abu Sufian Shanto <shantohridoy32@gmail.com>
2024-03-10 00:17:04 +0100

    Trying something new

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,46 @@
 import java.time.LocalDate;
 import java.time.Month;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         LocalDate localDate = LocalDate.now();
-        System.out.println("Today is: "+localDate);
-        DeutscheBahn db = new DeutscheBahn();
-        Train re3 = new Train("RE3", "Landshut");
-        Train re55 = new Train("RE55", "Tegernsee");
-        db.addTrain(re3);
-        db.addTrain(re55);
-        re3.setDepartureTime(23, 12);
-        re55.setDepartureTime(12, 43);
-        System.out.println(""+re3.getReTrain()+" departuring at "+re3.getDepartureTime()+" to "+re3.getTrainDestination());
-        System.out.println(""+re55.getReTrain()+" departuring at "+re55.getDepartureTime()+" to "+re55.getTrainDestination());
+        System.out.println("Today is "+localDate);
+        System.out.println("From: ");
+        Scanner input = new Scanner(System.in);
+        String from = input.nextLine();
+            if(fromCheck(from)==true) {
+                System.out.println("From: "+from);
+            } else {
+                throw new IllegalArgumentException("Input must contain only characters");
+            }
+       String to = input.nextLine();
+            if(toCheck(to)==true) {
+                System.out.println("To: "+to);
+            } else {
+                throw new IllegalArgumentException("Input must contain only characters");
+            }
 
 
     }
+    public static boolean fromCheck(String from) {
+        for (int i = 0; i < from.length(); i++) {
+            char ch = from.charAt(i);
+            if (!Character.isLetter(ch)) { // Check if the character is a letter
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean toCheck(String to) {
+        for (int i = 0; i < to.length(); i++) {
+            char ch = to.charAt(i);
+            if (!Character.isLetter(ch)) { // Check if the character is a letter
+                return false;
+            }
+        }
+        return true;
+    }
 }
+
